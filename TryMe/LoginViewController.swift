63bad5b27201e827f9ebe69fa6a7583a8a699279
Rushani Shawuti <ruhsane924@gmail.com>
2019-02-24 18:42:26 -0800

    regex email verification added

--- TryMe/LoginViewController.swift
+++ TryMe/LoginViewController.swift
@@ -17,126 +17,19 @@ class LoginViewController: UIViewController {
     
     
     @IBAction func SignUpTapped(_ sender: Any) {
-//        postRequest(url: "https://proj-fash.herokuapp.com/api/auth/signup")
         
-        let parameters = ["email": emailTextField.text!, "password": passwordTextField.text!]
-        
-        guard let url = URL(string: "https://proj-fash.herokuapp.com/api/auth/signup") else { return }
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        //make our parameter a json object
-        guard let httpBody =  try? JSONSerialization.data(withJSONObject: parameters, options: []) else { return }
-        request.httpBody = httpBody
-        
-        let session = URLSession.shared
-        session.dataTask(with: request) { (data, response, error) in
-            if let response = response {
-                print(response)
-            }
+        let validation = validateEmail(email: emailTextField.text ?? "")
+        if validation == true {
+            // call the back end sign up process . (back end gives back both if it passed back end side's validation process AND successfully or not it went through sign up process(which already takes care of going to the main screen if successful)
             
-            if let data = data {
-                do{
-                    let json = try JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
-                    print(json)
-                    let signupMessage = json["signup"] as! String
-                    print("HEREEEEEEEEE!", signupMessage)
-                    if signupMessage == "success" {
-                        DispatchQueue.main.async {
-                            let vc = self.storyboard?.instantiateViewController(withIdentifier: "main") as! ViewController
-                            self.present(vc, animated: true, completion: nil)
-                            //TODO: SAVE THE TOKEN TO USER DEFAULT
-                        }
-                    } else if signupMessage == "fail"  {
+            let statusSignUpProcess = postRequest(url: "https://proj-fash.herokuapp.com/api/auth/signup", callName: "signUp")
 
-                        DispatchQueue.main.async {
-                            let alert = UIAlertController(title: "Email already in use", message: "Email you have entered has already signed up!", preferredStyle: UIAlertController.Style.alert)
-                            alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
-                            self.present(alert, animated: true, completion: nil)
-                        }
-
-                    } else {
-                        DispatchQueue.main.async {
-                            let alert = UIAlertController(title: "Sign Up Failed", message: "Sorry. Unexpected sign up error, please try again", preferredStyle: UIAlertController.Style.alert)
-                            alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
-                            self.present(alert, animated: true, completion: nil)
-                        }
-                    }
-                }
-                catch{
-                    print(error)
-                }
-            }
-            
-            }.resume()
-        
-    }
-    
-    @IBAction func LoginTapped(_ sender: Any) {
-//        postRequest(url: "https://proj-fash.herokuapp.com/api/auth/login")
-        
-        let parameters = ["email": emailTextField.text!, "password": passwordTextField.text!]
-        
-        guard let url = URL(string: "https://proj-fash.herokuapp.com/api/auth/login") else { return }
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        //make our parameter a json object
-        guard let httpBody =  try? JSONSerialization.data(withJSONObject: parameters, options: []) else { return }
-        request.httpBody = httpBody
-        
-        let session = URLSession.shared
-        session.dataTask(with: request) { (data, response, error) in
-            if let response = response {
-                print(response)
-            }
-            
-            if let data = data {
-                do{
-                    let json = try JSONSerialization.jsonObject(with: data, options: []) as! [String: String]
-                    print(json)
-//                    if json[login] == success {
-//
-//                    }
-                    
-                    
-                    if json["login"] == "success" {
-                        DispatchQueue.main.async {
-                            let vc = self.storyboard?.instantiateViewController(withIdentifier: "main") as! ViewController
-                            self.present(vc, animated: true, completion: nil)
-                            //TODO: SAVE THE TOKEN TO USER DEFAULT
-                        }
-                    } else if json["login"] == "fail"  {
-                        
-                        DispatchQueue.main.async {
-                            let alert = UIAlertController(title: "Invalid username/password", message: "Invalid username/password", preferredStyle: UIAlertController.Style.alert)
-                            alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
-                            self.present(alert, animated: true, completion: nil)
-                        }
-                        
-                    } else {
-                        DispatchQueue.main.async {
-                            let alert = UIAlertController(title: "Login Failed", message: "Unexpected Login Error, please try again", preferredStyle: UIAlertController.Style.alert)
-                            alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
-                            self.present(alert, animated: true, completion: nil)
-                        }
-                    }
-                        
-                }
-                catch{
-                    print(error)
-                }
-            }
-            
-            }.resume()
-        
-    }
-//
-//    func postRequest(url: String) {
+        }
+ 
 //
 //        let parameters = ["email": emailTextField.text!, "password": passwordTextField.text!]
 //
-//        guard let url = URL(string: url) else { return }
+//        guard let url = URL(string: "https://proj-fash.herokuapp.com/api/auth/signup") else { return }
 //        var request = URLRequest(url: url)
 //        request.httpMethod = "POST"
 //        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
@@ -152,8 +45,31 @@ class LoginViewController: UIViewController {
 //
 //            if let data = data {
 //                do{
-//                    let json = try JSONSerialization.jsonObject(with: data, options: [])
+//                    let json = try JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
 //                    print(json)
+//                    let signupMessage = json["signup"] as! String
+//                    print("HEREEEEEEEEE!", signupMessage)
+//                    if signupMessage == "success" {
+//                        DispatchQueue.main.async {
+//                            let vc = self.storyboard?.instantiateViewController(withIdentifier: "main") as! ViewController
+//                            self.present(vc, animated: true, completion: nil)
+//                            //TODO: SAVE THE TOKEN TO USER DEFAULT
+//                        }
+//                    } else if signupMessage == "fail"  {
+//
+//                        DispatchQueue.main.async {
+//                            let alert = UIAlertController(title: "Email already in use", message: "Email you have entered has already signed up!", preferredStyle: UIAlertController.Style.alert)
+//                            alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+//                            self.present(alert, animated: true, completion: nil)
+//                        }
+//
+//                    } else {
+//                        DispatchQueue.main.async {
+//                            let alert = UIAlertController(title: "Sign Up Failed", message: "Sorry. Unexpected sign up error, please try again", preferredStyle: UIAlertController.Style.alert)
+//                            alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+//                            self.present(alert, animated: true, completion: nil)
+//                        }
+//                    }
 //                }
 //                catch{
 //                    print(error)
@@ -161,8 +77,135 @@ class LoginViewController: UIViewController {
 //            }
 //
 //            }.resume()
+//
 //    }
+        }
     
+    
+    @IBAction func LoginTapped(_ sender: Any) {
+        let statusLoginProcess = postRequest(url: "https://proj-fash.herokuapp.com/api/auth/login")
+        if statusLoginProcess == true {
+                let json = try JSONSerialization.jsonObject(with: data, options: []) as! [String: String]
+                print(json)
+                
+                if json["login"] == "success" {
+                    DispatchQueue.main.async {
+                        let vc = self.storyboard?.instantiateViewController(withIdentifier: "main") as! ViewController
+                        self.present(vc, animated: true, completion: nil)
+                        //TODO: SAVE THE TOKEN TO USER DEFAULT
+                    }
+                } else if json["login"] == "fail"  {
+                    
+                    DispatchQueue.main.async {
+                        let alert = UIAlertController(title: "Invalid username/password", message: "Invalid username/password", preferredStyle: UIAlertController.Style.alert)
+                        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+                        self.present(alert, animated: true, completion: nil)
+                    }
+                    
+                } else {
+                    DispatchQueue.main.async {
+                        let alert = UIAlertController(title: "Login Failed", message: "Unexpected Login Error, please try again", preferredStyle: UIAlertController.Style.alert)
+                        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+                        self.present(alert, animated: true, completion: nil)
+                    }
+                }
+                
+            } else if statusLoginProcess == false{
+                return
+    }
+        
+//        let parameters = ["email": emailTextField.text!, "password": passwordTextField.text!]
+//
+//        guard let url = URL(string: "https://proj-fash.herokuapp.com/api/auth/login") else { return }
+//        var request = URLRequest(url: url)
+//        request.httpMethod = "POST"
+//        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+//        //make our parameter a json object
+//        guard let httpBody =  try? JSONSerialization.data(withJSONObject: parameters, options: []) else { return }
+//        request.httpBody = httpBody
+//
+//        let session = URLSession.shared
+//        session.dataTask(with: request) { (data, response, error) in
+//            if let response = response {
+//                print(response)
+//            }
+//
+//            if let data = data {
+//                do{
+//                    let json = try JSONSerialization.jsonObject(with: data, options: []) as! [String: String]
+//                    print(json)
+//
+//                    if json["login"] == "success" {
+//                        DispatchQueue.main.async {
+//                            let vc = self.storyboard?.instantiateViewController(withIdentifier: "main") as! ViewController
+//                            self.present(vc, animated: true, completion: nil)
+//                            //TODO: SAVE THE TOKEN TO USER DEFAULT
+//                        }
+//                    } else if json["login"] == "fail"  {
+//
+//                        DispatchQueue.main.async {
+//                            let alert = UIAlertController(title: "Invalid username/password", message: "Invalid username/password", preferredStyle: UIAlertController.Style.alert)
+//                            alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+//                            self.present(alert, animated: true, completion: nil)
+//                        }
+//
+//                    } else {
+//                        DispatchQueue.main.async {
+//                            let alert = UIAlertController(title: "Login Failed", message: "Unexpected Login Error, please try again", preferredStyle: UIAlertController.Style.alert)
+//                            alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+//                            self.present(alert, animated: true, completion: nil)
+//                        }
+//                    }
+//
+//                }
+//                catch{
+//                    print(error)
+//                }
+//            }
+//
+//            }.resume()
+//
+    }
+    
+    func validateEmail(email: String)  -> Bool {
+        let emailRegEx = "^[a-zA-Z0-9.!#$%&'*+=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)"
+        
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailTest.evaluate(with: email)
+    }
+
+    func postRequest(url: String, callName: String ){
+
+        let parameters = ["email": emailTextField.text!, "password": passwordTextField.text!]
+
+        var request = URLRequest(url: URL(string: url)!)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        //make our parameter a json object
+        guard let httpBody =  try? JSONSerialization.data(withJSONObject: parameters, options: []) else { return }
+        request.httpBody = httpBody
+
+        let session = URLSession.shared
+        session.dataTask(with: request) { (data, response, error) in
+            if let response = response {
+                print(response)
+            }
+
+            if let data = data {
+                do{
+                    let json = try JSONSerialization.jsonObject(with: data, options: [])
+                    print(json)
+                    return true
+                }
+                catch{
+                    print(error)
+                    return false
+                }
+            }
+
+            }.resume()
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
