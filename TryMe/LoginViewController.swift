f3301e2e1245cd60939039ba19110beb1bd533f0
Rushani Shawuti <ruhsane924@gmail.com>
2019-03-06 07:24:35 -0800

    view goes up when keyboard appears

--- TryMe/LoginViewController.swift
+++ TryMe/LoginViewController.swift
@@ -194,10 +194,24 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
         UIDesign()
         SetupConstraints()
-    
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
 
-
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0 {
+                self.view.frame.origin.y -= keyboardSize.height
+            }
+        }
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
+    }
+        
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
     }
